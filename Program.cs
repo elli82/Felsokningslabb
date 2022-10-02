25e0e5da47945272c598748ca1295de6c466da37
Elin Linderholm <elin.otterdahl@hotmail.com>
2022-10-02 18:18:02 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Felsökningslabb  // Elin Linderholm SUT22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //case 1
+            //int number = 2;
+
+            //if (number > 3)
+            //{
+            //    Console.WriteLine("Talet är större än tre"); // inget semikolon
+            //}
+            //else if (number < 3)        //mellanslag saknades mellan else och if
+            //{
+            //    Console.WriteLine("Talet är mindre än tre");
+            //}
+
+            // case 2
+            //for (int i = 1; i <= 100; i++)  // ändrade operatorn till <= 100
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            // case 3
+            //for (int i = 1; i <= 5; i++)  // inget ++
+            //{
+            //    for (int j = 1; j <= i; j++)  //inget ++
+            //    {
+            //        Console.Write(j + " ");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            // case 4
+            //int i = 1;
+            //while (i <= 5)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("x");   // parentesen innehöll inget värde att skriva ut
+            //    }
+
+            //    Console.WriteLine();
+            //    i++;
+            //}
+
+            // case 5
+            //int i = 1;
+
+            //switch (i)
+            //{
+            //    case 1:
+            //        Console.WriteLine();   //lägger till en tom rad i case 1 och programmet visar tomt
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Two");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Other");
+            //        break;
+            //}
+
+            // case 6
+            //int i = 10;
+
+            //if (i == 5)   // behövs två =
+            //{
+            //    Console.WriteLine("i är 5");
+            //}
+
+        }
+    }
+}
